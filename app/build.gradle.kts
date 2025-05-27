1f2acad8f3b5c61f4f8d20fe1621bc24ce6b5b3e
bruno <bruno13palhano@gmail.com>
2025-05-27 10:42:04 -0300

    Add nav3 snapshot

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
     namespace = "com.bruno13palhano.expensis"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.bruno13palhano.expensis"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
