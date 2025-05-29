69c4b3f58ac8864a0642114d5e6ff77467a0f7e0
bruno <bruno13palhano@gmail.com>
2025-05-29 17:58:25 -0300

    Add ExpenseDao

---
+++ core/src/main/java/com/bruno13palhano/core/data/dao/ExpenseDao.kt
@@ -0,0 +1,27 @@
+package com.bruno13palhano.core.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.bruno13palhano.core.data.model.ExpenseEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+internal interface ExpenseDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(expense: ExpenseEntity): Long
+
+    @Update
+    suspend fun update(expense: ExpenseEntity): Int
+
+    @Query("DELETE FROM Expense WHERE id = :id")
+    suspend fun delete(id: Long): Int
+
+    @Query("SELECT * FROM Expense WHERE id = :id")
+    suspend fun getById(id: Long): ExpenseEntity?
+
+    @Query("SELECT * FROM Expense")
+    fun getAll(): Flow<List<ExpenseEntity>>
+}
