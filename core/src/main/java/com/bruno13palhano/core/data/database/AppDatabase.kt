84d4e4f99c7b778ffb03f108aafef72258dc17d5
bruno <bruno13palhano@gmail.com>
2025-05-29 18:00:05 -0300

    Add database

---
+++ core/src/main/java/com/bruno13palhano/core/data/database/AppDatabase.kt
@@ -0,0 +1,17 @@
+package com.bruno13palhano.core.data.database
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.bruno13palhano.core.data.dao.ExpenseDao
+import com.bruno13palhano.core.data.model.ExpenseEntity
+
+@Database(
+    entities = [
+        ExpenseEntity::class
+    ],
+    version = 1,
+    exportSchema = false,
+)
+internal abstract class AppDatabase : RoomDatabase() {
+    abstract fun expenseDao(): ExpenseDao
+}
---
+++ core/src/main/java/com/bruno13palhano/core/data/di/DatabaseModule.kt
@@ -0,0 +1,31 @@
+package com.bruno13palhano.core.data.di
+
+import android.content.Context
+import androidx.room.Room
+import com.bruno13palhano.core.data.database.AppDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideExpenseDao(appDatabase: AppDatabase)  = appDatabase.expenseDao()
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(@ApplicationContext context: Context): AppDatabase {
+        return Room.databaseBuilder(
+            context,
+            AppDatabase::class.java,
+            "app_db"
+        )
+            .fallbackToDestructiveMigration(false)
+            .build()
+    }
+}
