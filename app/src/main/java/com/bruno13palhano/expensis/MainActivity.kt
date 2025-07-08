be2d3f907a74f615cd155bb4e5f64ffa6fe5534e
bruno <bruno13palhano@gmail.com>
2025-07-08 14:32:51 -0300

    Add application class for hilt DI

--- app/src/main/java/com/bruno13palhano/expensis/MainActivity.kt
+++ app/src/main/java/com/bruno13palhano/expensis/MainActivity.kt
@@ -10,7 +10,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.bruno13palhano.expensis.ui.navigation.AppNavigation
 import com.bruno13palhano.expensis.ui.theme.ExpensisTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/bruno13palhano/expensis/ui/app/ExpensisApp.kt
@@ -0,0 +1,7 @@
+package com.bruno13palhano.expensis.ui.app
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ExpensisApp : Application()
