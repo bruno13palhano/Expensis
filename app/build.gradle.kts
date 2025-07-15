c6525cbeeded0600b26fad154f78f83282f5ee42
bruno <bruno13palhano@gmail.com>
2025-07-15 12:33:52 -0300

    Add hilt navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,7 @@ dependencies {
     implementation(libs.androidx.navigation3.ui)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+    implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.androidx.material3.navigation3)
     implementation(libs.kotlinx.serialization.core)
     implementation(libs.kotlinx.serialization.json)
