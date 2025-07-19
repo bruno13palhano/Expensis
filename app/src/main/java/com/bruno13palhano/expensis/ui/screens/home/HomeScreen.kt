dfddfb9409902790d792f326823ee02ab534a858
bruno <bruno13palhano@gmail.com>
2025-07-19 15:34:06 -0300

    Show a list with all voice commands

--- app/src/main/java/com/bruno13palhano/expensis/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/bruno13palhano/expensis/ui/screens/home/HomeScreen.kt
@@ -6,16 +6,30 @@ import android.content.Intent
 import android.speech.RecognizerIntent
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Mic
+import androidx.compose.material.icons.outlined.Visibility
+import androidx.compose.material.icons.outlined.VisibilityOff
+import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -26,10 +40,12 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.bruno13palhano.expensis.R
@@ -99,12 +115,12 @@ fun HomeScreen(
                         )
                     }
                 }
-                is HomeSideEffect.Command -> {
+                is HomeSideEffect.NavigateTo -> {
                     when (effect.command) {
                         HomeCommand.NEW_EXPENSE -> navigateToNewExpense()
                         HomeCommand.EXPENSES -> navigateToExpenses()
                         HomeCommand.ANALYTICS -> navigateToAnalytics()
-                        HomeCommand.ERROR -> {
+                        else -> {
                             scope.launch {
                                 snackbarHostState.showSnackbar(
                                     message = unknownCommandMessage,
@@ -127,7 +143,7 @@ fun HomeScreen(
 
 private fun getHomeCommand(context: Context, recognizedText: String?): HomeCommand {
     if (recognizedText?.contains(
-            other = context.getString(R.string.expense_command),
+            other = context.getString(R.string.new_expense_command),
             ignoreCase = true,
         ) == true
     ) {
@@ -146,6 +162,13 @@ private fun getHomeCommand(context: Context, recognizedText: String?): HomeComma
         ) == true
     ) {
         return HomeCommand.ANALYTICS
+    }
+    if (recognizedText?.contains(
+            other = context.getString(R.string.show_commands),
+            ignoreCase = true,
+        ) == true
+    ) {
+        return HomeCommand.COMMANDS
     }
 
     return HomeCommand.ERROR
@@ -174,6 +197,123 @@ private fun HomeContent(
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
     ) {
         Column(modifier = Modifier.padding(it)) {
+            ListItem(
+                headlineContent = {
+                    val profit = if (state.isProfitVisible) {
+                        state.profit.toString()
+                    } else {
+                        stringResource(id = R.string.dots)
+                    }
+
+                    Text(text = profit)
+                },
+                trailingContent = {
+                    IconButton(onClick = { onEvent(HomeEvent.ToggleProfitVisibility) }) {
+                        if (state.isProfitVisible) {
+                            Icon(
+                                imageVector = Icons.Outlined.Visibility,
+                                contentDescription = stringResource(
+                                    id = R.string.profit_visibility_on,
+                                ),
+                            )
+                        } else {
+                            Icon(
+                                imageVector = Icons.Outlined.VisibilityOff,
+                                contentDescription = stringResource(
+                                    id = R.string.profit_visibility_off,
+                                ),
+                            )
+                        }
+                    }
+                },
+            )
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Box(
+                    modifier = Modifier
+                        .padding(start = 8.dp, top = 8.dp, end = 4.dp, bottom = 4.dp)
+                        .sizeIn(minHeight = 200.dp)
+                        .weight(1f)
+                        .clickable { onEvent(HomeEvent.NavigateToNewExpense) },
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(text = stringResource(id = R.string.new_expense))
+                }
+
+                Box(
+                    modifier = Modifier
+                        .padding(start = 4.dp, top = 8.dp, end = 8.dp, bottom = 4.dp)
+                        .sizeIn(minHeight = 200.dp)
+                        .weight(1f)
+                        .clickable { onEvent(HomeEvent.NavigateToExpenses) },
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.expenses),
+                    )
+                }
+            }
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Box(
+                    modifier = Modifier
+                        .padding(start = 8.dp, top = 4.dp, end = 4.dp, bottom = 8.dp)
+                        .sizeIn(minHeight = 200.dp)
+                        .weight(1f)
+                        .clickable { onEvent(HomeEvent.NavigateToAnalytics) },
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(text = stringResource(id = R.string.analytics))
+                }
+
+                Box(
+                    modifier = Modifier
+                        .padding(start = 4.dp, top = 4.dp, end = 8.dp, bottom = 8.dp)
+                        .sizeIn(minHeight = 200.dp)
+                        .weight(1f)
+                        .clickable { onEvent(HomeEvent.ToggleCommandsVisibility) },
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.commands),
+                    )
+                }
+            }
+        }
+
+        if (state.isCommandsInfoVisible) {
+            Card(
+                modifier = Modifier
+                    .padding(it)
+                    .padding(vertical = 88.dp, horizontal = 16.dp)
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
+                onClick = { onEvent(HomeEvent.ToggleCommandsVisibility) },
+            ) {
+                Text(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.CenterHorizontally),
+                    text = "All Commands",
+                    style = MaterialTheme.typography.titleLarge
+                )
+
+                val commandsInfo = listOf(
+                    "1. ${stringResource(id = R.string.new_expense_command)} ->" +
+                        " ${stringResource(id = R.string.new_expense_command_info)}",
+                    "2. ${stringResource(id = R.string.expenses_command)} ->" +
+                        " ${stringResource(id = R.string.expenses_command_info)}",
+                    "3. ${stringResource(id = R.string.analytics_command)} ->" +
+                        " ${stringResource(id = R.string.analytics_command_info)}",
+                    "4. ${stringResource(id = R.string.show_commands)} ->" +
+                        " ${stringResource(id = R.string.commands_info)}",
+                )
+
+                commandsInfo.forEach { info ->
+                    Text(
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+                        text = info,
+                    )
+                }
+            }
         }
     }
 }
@@ -187,6 +327,22 @@ private fun HomePreview() {
             state = HomeState(
                 profit = 1000.0,
                 isProfitVisible = true,
+            ),
+            onEvent = {},
+        )
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+private fun HomeVisibilityOffPreview() {
+    ExpensisTheme {
+        HomeContent(
+            snackbarHostState = SnackbarHostState(),
+            state = HomeState(
+                profit = 1000.0,
+                isProfitVisible = false,
+                isCommandsInfoVisible = true,
             ),
             onEvent = {},
         )
